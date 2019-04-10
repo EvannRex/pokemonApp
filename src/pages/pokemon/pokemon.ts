c704c18b670f4a74c5f4fb5b4a28a66d7d33fab2
EvannyR <evanny.rira@outlook.com>
2019-04-10 18:11:03 -0500

    debilidades y fortalezas incluidas

--- src/pages/pokemon/pokemon.ts
+++ src/pages/pokemon/pokemon.ts
@@ -13,6 +13,8 @@ export class PokemonPage {
   altura:any;
   imagen:any;
   tipos:any;
+  fortalezas: any= new Array();
+  debilidades: any= new Array();
   constructor(public navCtrl: NavController,
     private pokemonProvider: PokemonProvider, 
     public navParams: NavParams) {
@@ -20,16 +22,33 @@ export class PokemonPage {
   }
 
   ionViewDidLoad() {
-    //console.log('ionViewDidLoad PokemonPage');
+    console.log('ionViewDidLoad PokemonPage');
     this.getInfo();
   }
   getInfo() {
+    let index: number=0;
     this.pokemonProvider.getImgUrl(this.pokemon.url).subscribe(
       (response: any) => {
         this.peso= response.weight;
         this.imagen=response.sprites.front_default;
         this.altura=response.height;
         this.tipos=response['types'];
+        while(index < this.tipos.length){
+          this.pokemonProvider.getImgUrl(this.tipos[index].type.url).subscribe(
+            (response2:any)=>{
+              for (let index = 0; index < response2.damage_relations.double_damage_from.length; index++) {
+                this.debilidades.push(response2.damage_relations.double_damage_from[index])
+              }
+              for (let index = 0; index < response2.damage_relations.double_damage_to.length; index++) {
+                this.fortalezas.push(response2.damage_relations.double_damage_to[index])
+              }
+              
+            },(error2)=>{
+              console.log(error2);
+            })
+          index=index+1;
+        };
+
         
       }, (error) => {
         console.log(error);
